507721f01b918c9aa9c0d64225be53feeb671e19
Asim - HP PC <asimfiaz97@gmail.com>
2023-04-13 05:22:18 +0500

    added tic tac toe and who's that pokemon

--- bot.js
+++ bot.js
@@ -10,9 +10,8 @@ import { commands } from './commands.js';
 
 // Importing games
 import { rockPaperScissors } from './games/rockpaperscissor.js';
-import { playHangman } from './games/hangman.js';
 
-import { Hangman } from 'discord-gamecord';
+import { Hangman, TicTacToe, GuessThePokemon } from 'discord-gamecord';
 
 client.on('ready', () => {
     console.log("Logged in as " + client.user.tag);
@@ -22,9 +21,9 @@ const rest = new REST({ version: '10' }).setToken(TOKEN);
 
 (async () => {
     try {
-        //console.log('Started refreshing application (/) commands.');
+        console.log('Started refreshing application (/) commands.');
         await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
-        //console.log('Successfully reloaded application (/) commands.');
+        console.log('Successfully reloaded application (/) commands.');
     } catch (e) {
         console.error(e);
     }
@@ -33,17 +32,13 @@ const rest = new REST({ version: '10' }).setToken(TOKEN);
 // All slash commands can be configured to trigger here.
 client.on('interactionCreate', async interaction => {
     if (!interaction.isChatInputCommand()) return;
-    
-    if (interaction.commandName === 'test') {
-        await interaction.reply("I am working now!");
-    }
 
     if (interaction.commandName === 'rockpaperscissors') {
         await interaction.reply(rockPaperScissors(interaction.options.data[0].value));
     }
 
     if (interaction.commandName === 'hangman') {
-        const Game = new Hangman({
+        let Game = new Hangman({
             message: interaction,
             embed: {
                 title: 'Hangman game',
@@ -55,6 +50,61 @@ client.on('interactionCreate', async interaction => {
         Game.startGame();
         Game.on('gameOver', async result => {
             console.log(result);
+        });
+    }
+
+    if (interaction.commandName === 'whosthatpokemon') {
+        let Game = new GuessThePokemon({
+            message: interaction,
+            isSlashGame: true,
+            embed: {
+                title: 'Who\'s That Pokemon',
+                color: '#5865F2'
+            },
+            timeoutTime: 60000,
+            winMessage: 'You guessed it right! It was a {pokemon}.',
+            loseMessage: 'Better luck next time! It was a {pokemon}.',
+            errMessage: 'Unable to fetch pokemon data! Please try again.',
+            playerOnlyMessage: 'Only {player} can use these buttons.'
+        });
+
+        Game.startGame();
+        Game.on('gameOver', result => {
+            //console.log(result);  // =>  { result... }
+        });
+    }
+
+
+    if (interaction.commandName === 'tictactoe') {
+        let Game = new TicTacToe({
+            message: interaction,
+            isSlashGame: true,
+            opponent: interaction.options.getUser('user'),
+            embed: {
+                title: 'Tic Tac Toe',
+                color: '#5865F2',
+                statusTitle: 'Status',
+                overTitle: 'Game Over'
+            },
+            emojis: {
+                xButton: '❌',
+                oButton: '🔵',
+                blankButton: '➖'
+            },
+            mentionUser: true,
+            timeoutTime: 60000,
+            xButtonStyle: 'DANGER',
+            oButtonStyle: 'PRIMARY',
+            turnMessage: '{emoji} | Its turn of player **{player}**.',
+            winMessage: '{emoji} | **{player}** won the TicTacToe Game.',
+            tieMessage: 'The Game tied! No one won the Game!',
+            timeoutMessage: 'The Game went unfinished! No one won the Game!',
+            playerOnlyMessage: 'Only {player} and {opponent} can use these buttons.'
+        });
+
+        Game.startGame();
+        Game.on('gameOver', result => {
+            //console.log(result);  // =>  { result... }
         });
     }
 
@@ -77,7 +127,7 @@ client.on('messageCreate', (message) => {
         message.channel.send({ content: "CANON!", files: ["./img/satosere.gif"] });
         return;
     }
-    
+
     msg = message.content.toLowerCase();
 
     if (msg.includes("abu dhabi 2021")) {
--- commands.js
+++ commands.js
@@ -1,8 +1,4 @@
 export const commands = [
-    {
-        name: 'test',
-        description: 'Replies with test is working'
-    },
     // Play Rock, Paper, Scissor
     {
         name: 'rockpaperscissors',
@@ -35,5 +31,23 @@ export const commands = [
         name: 'hangman',
         description: 'Play hangman',
         type: 1,
-    }
+    },
+    {
+        name: 'whosthatpokemon',
+        description: 'Play Who\'s that Pokemon',
+        type: 1,
+    },
+    {
+        name: 'tictactoe',
+        description: 'Play Tic Tac Toe',
+        type: 1,
+        options: [
+            {
+                name: 'opponent',
+                type: 1,
+                required: false,
+                description: 'Your opponent',
+            }
+        ]
+    },
 ];
